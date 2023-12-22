428ca6c3834e3b009702b5f5eb710a48e087c980
Arif.dev.ops <reza.jmi.ece@gmail.com>
2023-12-22 14:51:38 +0000

    of two given number

---
+++ arithmetic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#calculation of two number which is defined in script
+
+A=12
+B=20
+echo "Sum is $((A + b))"
+echo "Difference is $((A -B))"
+echo "Product is $((A * B))"
+echo "Quotient is *((A / B))"
+
